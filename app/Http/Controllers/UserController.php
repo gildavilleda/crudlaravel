b336fbd64e6e305b61c0d1417f517cc79b81d448
Gilda Villeda <gildavilleda@gmail.com>
2022-04-17 20:42:43 -0600

    actualizar repositorio

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\user;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::latest()->get();
+
+        return view('users.index',[
+            'users' => $users
+        ]);
+    }
+    public function store(Request $request)
+    {
+        User::create([
+            'name'=> $request-> name,
+            'email'=> $request-> email,
+            'password'=> $request-> password,
+        ]);
+        return back();
+    }
+    public function destroy(User $user)
+    {
+            $User->delte();
+            return back();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/','UserController@index');
+//Route::get('/',[UserController::class, 'index']);
+//Route::post('users', 'UserController@store')->name('users.store');
+//Route::delete('users/{user}','UserController@destroy')->name('users.destroy');
+
+
+Route::get('/', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
